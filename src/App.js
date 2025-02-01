590cd90c5675a5ce200901c53819889363ca71f7
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-02-01 11:33:16 +0530

    navbar brand change in text

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <Link to="/" className="navbar-brand"><img src={logo} alt="logo" width="30" /> RaptorFormula</Link>
+            <Link to="/" className="navbar-brand"><img src={logo} alt="logo" width="30" /> Raptor<em style={{color: "#fe3021"}}>F1</em></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
